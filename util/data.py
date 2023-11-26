9f6da74751e5ea43091829b82d7f5d5a3e432e5f
M4rkX-Y <markx2002@gmail.com>
2023-11-26 12:20:58 -0500

    Update data.py

--- util/data.py
+++ util/data.py
@@ -1,25 +1,42 @@
 import json
 
 
-class DataExtractor:
+class Data:
     def __init__(self, file_path):
         self.file_path = file_path
+        raw_data = self.read_file()
+        data = Butlr32_Parser().parse_raw(raw_data)
+        self.data = data
 
     def read_file(self):
         with open(self.file_path, "r") as file:
             return file.read()
 
-    def extract_data(self):
-        raise NotImplementedError
+    def pop_frame(self):
+        raw_frame = self.data.pop(0)
+        frame = Butlr32_Parser().parse_frame(raw_frame)
+        return frame
+
+    def get_length(self):
+        return len(self.data)
 
 
-class TxtDataExtractor(DataExtractor):
-    def extract_data(self):
-        content = self.read_file()
-        return content
+class Butlr32_Parser:
+    def parse_raw(self, raw_data):
+        if not raw_data:
+            raise Exception
+        filtered_data = [
+            data for data in raw_data.replace(", b", "").split('"')[1:] if data
+        ]
+        return filtered_data
+
+    def parse_frame(self, raw_frame):
+        clean_frame = (
+            raw_frame.replace(",)", "")[1:].replace("'", '"').replace("       ", "")
+        )
+        frame = json.loads(clean_frame.split("array(")[1].split("),")[0])
+        return frame
 
 
-class JsonDataExtractor(DataExtractor):
-    def extract_data(self):
-        content = self.read_file()
-        return json.loads(content)
+txt = Data("data\\standing_9_32x32_sensor.txt")
+print(txt.pop_frame())
