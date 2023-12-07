69d25f087cc555166aff410f65c14edad4af7a81
M4rkX-Y <markx2002@gmail.com>
2023-12-06 23:58:23 -0500

    finalize

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import pygame
 import sys
 import pygame_gui
 from tkinter import filedialog
-from scipy.ndimage import zoom
 from data_structure import Butlr32_Data, Esoil_Data
+from visualizer import PixelMap, SmoothPixelMap
 
 
 SCREEN_SIZE = 650, 750
-DISPLAY_SIZE = 576
-DISPLAY_POSITION = 40, 120
 OPENFILE_BUTTON = (40, 40), (120, 40)
 PLAY_BUTTON = (210, 40), (100, 40)
 PAUSE_BUTTON = (350, 40), (100, 40)
@@ -19,53 +17,10 @@ GRAY = 53, 53, 53
 FPS = 5
 
 
-class Pixel:
-    def __init__(self, x, y, color, pixel_size):
-        self.rect = pygame.Rect(x, y, pixel_size, pixel_size)
-        self.color = color
-
-    def draw(self, surface):
-        pygame.draw.rect(surface, self.color, self.rect)
-
-
-class PixelMap:
-    def __init__(self, pixel_map):
-        self.pixels = []
-        self.size = len(pixel_map)
-        self.pixel_size = DISPLAY_SIZE / self.size
-
-        for row in range(self.size):
-            for col in range(self.size):
-                x = col * self.pixel_size + DISPLAY_POSITION[0]
-                y = row * self.pixel_size + DISPLAY_POSITION[1]
-                color = tuple([pixel_map[row][col] * x for x in WHITE])
-                self.pixels.append(Pixel(x, y, color, self.pixel_size))
-
-    def draw(self, surface):
-        for pixel in self.pixels:
-            pixel.draw(surface)
-
-
-class SmoothPixelMap(PixelMap):
-    def __init__(self, pixel_map):
-        super().__init__(pixel_map)
-        self.pixels = []
-        self.size *= 2
-        self.pixel_size = DISPLAY_SIZE / self.size
-        smooth_pixel_map = zoom(pixel_map, zoom=2, order=1)
-        for row in range(self.size):
-            for col in range(self.size):
-                x = col * self.pixel_size + DISPLAY_POSITION[0]
-                y = row * self.pixel_size + DISPLAY_POSITION[1]
-                color = tuple([smooth_pixel_map[row][col] * x for x in WHITE])
-                self.pixels.append(Pixel(x, y, color, self.pixel_size))
-
-
 def main():
     pygame.init()
     pygame.display.set_caption("Data Visualization")
 
-    # TODO: Adding the implementation of reading the file and select how we want to visualize the data.
     screen = pygame.display.set_mode(SCREEN_SIZE)
     manager = pygame_gui.UIManager(SCREEN_SIZE)
     play = False
---
+++ visualizer.py
@@ -0,0 +1,49 @@
+import pygame
+from scipy.ndimage import zoom
+
+
+DISPLAY_SIZE = 576
+DISPLAY_POSITION = 40, 120
+WHITE = 255, 255, 255
+
+
+class Pixel:
+    def __init__(self, x, y, color, pixel_size):
+        self.rect = pygame.Rect(x, y, pixel_size, pixel_size)
+        self.color = color
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect)
+
+
+class PixelMap:
+    def __init__(self, pixel_map):
+        self.pixels = []
+        self.size = len(pixel_map)
+        self.pixel_size = DISPLAY_SIZE / self.size
+
+        for row in range(self.size):
+            for col in range(self.size):
+                x = col * self.pixel_size + DISPLAY_POSITION[0]
+                y = row * self.pixel_size + DISPLAY_POSITION[1]
+                color = tuple([pixel_map[row][col] * x for x in WHITE])
+                self.pixels.append(Pixel(x, y, color, self.pixel_size))
+
+    def draw(self, surface):
+        for pixel in self.pixels:
+            pixel.draw(surface)
+
+
+class SmoothPixelMap(PixelMap):
+    def __init__(self, pixel_map):
+        super().__init__(pixel_map)
+        self.pixels = []
+        self.size *= 2
+        self.pixel_size = DISPLAY_SIZE / self.size
+        smooth_pixel_map = zoom(pixel_map, zoom=2, order=1)
+        for row in range(self.size):
+            for col in range(self.size):
+                x = col * self.pixel_size + DISPLAY_POSITION[0]
+                y = row * self.pixel_size + DISPLAY_POSITION[1]
+                color = tuple([smooth_pixel_map[row][col] * x for x in WHITE])
+                self.pixels.append(Pixel(x, y, color, self.pixel_size))
