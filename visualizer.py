e6c56a435add94d2f84b29500e953ef5a9b737eb
M4rkX-Y <markx2002@gmail.com>
2023-12-08 11:51:42 -0500

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -19,15 +19,19 @@ class Pixel:
 class PixelMap:
     def __init__(self, pixel_map):
         self.pixels = []
-        self.size = len(pixel_map)
+        modified_pixel_map = self.modification(pixel_map)
+        self.size = len(modified_pixel_map)
         self.pixel_size = DISPLAY_SIZE / self.size
 
         for row in range(self.size):
             for col in range(self.size):
                 x = col * self.pixel_size + DISPLAY_POSITION[0]
                 y = row * self.pixel_size + DISPLAY_POSITION[1]
-                color = tuple([pixel_map[row][col] * x for x in WHITE])
+                color = tuple([modified_pixel_map[row][col] * x for x in WHITE])
                 self.pixels.append(Pixel(x, y, color, self.pixel_size))
+
+    def modification(self, og_pixel_map):
+        return og_pixel_map
 
     def draw(self, surface):
         for pixel in self.pixels:
@@ -37,12 +41,6 @@ class PixelMap:
 class SmoothPixelMap(PixelMap):
     def __init__(self, pixel_map):
         super().__init__(pixel_map)
-        self.size *= 2
-        self.pixel_size = DISPLAY_SIZE / self.size
-        smooth_pixel_map = zoom(pixel_map, zoom=2, order=1)
-        for row in range(self.size):
-            for col in range(self.size):
-                x = col * self.pixel_size + DISPLAY_POSITION[0]
-                y = row * self.pixel_size + DISPLAY_POSITION[1]
-                color = tuple([smooth_pixel_map[row][col] * x for x in WHITE])
-                self.pixels.append(Pixel(x, y, color, self.pixel_size))
+
+    def modification(self, og_pixel_map):
+        return zoom(og_pixel_map, zoom=2, order=1)
